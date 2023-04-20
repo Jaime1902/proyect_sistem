38b18fd7ab48690f98992e55173d4e07cc506571
Jaime1902 <112213180+Jaime1902@users.noreply.github.com>
2023-04-19 20:50:01 -0600

    Create project_db.sql

---
+++ project_db.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-04-2023 a las 04:36:38
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `project_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id_alumno` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `lugar_nacimiento` varchar(50) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `codigo_estudiante` varchar(20) NOT NULL,
+  `id_grado` int(11) NOT NULL,
+  `fecha_inscripcion` date NOT NULL,
+  `padecimiento_alergia` varchar(100) DEFAULT NULL,
+  `nombre_padre` varchar(50) NOT NULL,
+  `nombre_madre` varchar(50) NOT NULL,
+  `cedula_padre` varchar(20) NOT NULL,
+  `cedula_madre` varchar(20) NOT NULL,
+  `telefono_emergencia` varchar(20) NOT NULL,
+  `ocupacion_padre` varchar(50) NOT NULL,
+  `ocupacion_madre` varchar(50) NOT NULL,
+  `direccion_exacta` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignaturas`
+--
+
+CREATE TABLE `asignaturas` (
+  `id_asignatura` int(11) NOT NULL,
+  `nombre_asignatura` varchar(50) NOT NULL,
+  `id_grado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificaciones`
+--
+
+CREATE TABLE `calificaciones` (
+  `id_calificacion` int(11) NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `id_grado` int(11) NOT NULL,
+  `id_asignatura` int(11) NOT NULL,
+  `semestre1` float DEFAULT NULL,
+  `semestre2` float DEFAULT NULL,
+  `semestre3` float DEFAULT NULL,
+  `semestre4` float DEFAULT NULL,
+  `promedio_final` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grados`
+--
+
+CREATE TABLE `grados` (
+  `id_grado` int(11) NOT NULL,
+  `nombre_grado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `grados`
+--
+
+INSERT INTO `grados` (`id_grado`, `nombre_grado`) VALUES
+(1, '2 Nivel'),
+(2, '3 Nivel'),
+(3, '1 grado'),
+(4, '2 grado'),
+(5, '3 grado'),
+(6, '4 grado'),
+(7, '5 grado'),
+(8, '6 grado'),
+(9, '7 grado'),
+(10, '8 grado'),
+(11, '9 grado'),
+(12, '10 grado'),
+(13, '11 grado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varbinary(255) NOT NULL,
+  `role` enum('administrador','secretaria') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', 0x6d820a4ff9523eadb1f36b61a9ca0a77, 'administrador'),
+(2, 'secretaria', 0x9479d5c6b840b19a42c38ab6190a293c, 'secretaria');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensualidades`
+--
+
+CREATE TABLE `mensualidades` (
+  `id_mensualidad` int(11) NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `fecha_pago` date NOT NULL,
+  `monto` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id_alumno`),
+  ADD KEY `id_grado` (`id_grado`);
+
+--
+-- Indices de la tabla `asignaturas`
+--
+ALTER TABLE `asignaturas`
+  ADD PRIMARY KEY (`id_asignatura`),
+  ADD KEY `id_grado` (`id_grado`);
+
+--
+-- Indices de la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  ADD PRIMARY KEY (`id_calificacion`),
+  ADD KEY `id_alumno` (`id_alumno`),
+  ADD KEY `id_asignatura` (`id_asignatura`);
+
+--
+-- Indices de la tabla `grados`
+--
+ALTER TABLE `grados`
+  ADD PRIMARY KEY (`id_grado`);
+
+--
+-- Indices de la tabla `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `mensualidades`
+--
+ALTER TABLE `mensualidades`
+  ADD PRIMARY KEY (`id_mensualidad`),
+  ADD KEY `id_alumno` (`id_alumno`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `asignaturas`
+--
+ALTER TABLE `asignaturas`
+  MODIFY `id_asignatura` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  MODIFY `id_calificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grados`
+--
+ALTER TABLE `grados`
+  MODIFY `id_grado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `mensualidades`
+--
+ALTER TABLE `mensualidades`
+  MODIFY `id_mensualidad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD CONSTRAINT `alumnos_ibfk_1` FOREIGN KEY (`id_grado`) REFERENCES `grados` (`id_grado`);
+
+--
+-- Filtros para la tabla `asignaturas`
+--
+ALTER TABLE `asignaturas`
+  ADD CONSTRAINT `asignaturas_ibfk_1` FOREIGN KEY (`id_grado`) REFERENCES `grados` (`id_grado`);
+
+--
+-- Filtros para la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  ADD CONSTRAINT `calificaciones_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumnos` (`id_alumno`),
+  ADD CONSTRAINT `calificaciones_ibfk_2` FOREIGN KEY (`id_asignatura`) REFERENCES `asignaturas` (`id_asignatura`);
+
+--
+-- Filtros para la tabla `mensualidades`
+--
+ALTER TABLE `mensualidades`
+  ADD CONSTRAINT `mensualidades_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumnos` (`id_alumno`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
