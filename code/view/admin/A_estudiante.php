45fd113e729e28e0a8277a312217173cc6242234
Jaime1902 <112213180+Jaime1902@users.noreply.github.com>
2023-04-21 16:26:36 -0600

    0.0.3

---
+++ code/view/admin/A_estudiante.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="header">
+    <h1>Colegio Cristiano Presbiteriano</h1>
+  </div>
+  <div id="menu">
+    <a href="admin.php">Inicio</a>
+    <a href="#">Estudiantes</a>
+    <a href="#">Mensualidad</a>
+    <a href="#">Profesores</a>
+    <a href="#">Cursos</a>
+    <a href="#">Calificaciones</a>
+    <a href="close.php">Cerrar sesión</a>
+  </div>
+  <div id="content">
+    <div class="module module-student">
+      <a href="A_estudiante.php">
+        <img src="../../img/logo/graduado.png" alt="Estudiantes">
+        <h2>Estudiantes</h2>
+      </a>
+    </div>
+    <div class="module module-teacher">
+      <a href="#">
+        <img src="../../img/logo/instructor.png" alt="Profesores">
+        <h2>Profesores</h2>
+      </a>
+    </div>
+  </div>
+</body>
+</html>
--- code/view/admin/admin.php
+++ code/view/admin/admin.php
@@ -14,162 +14,15 @@ if (!isset($_SESSION['username']) || ($_SESSION['role'] != 'administrador' && $_
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
-  <style type="text/css">
-    body {
-      margin: 0;
-      padding: 0;
-      font-family: sans-serif;
-      background-color: #fff;
-      color: #333;
-    }
-
-    #header {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      height: 60px;
-      padding: 0 20px;
-      background-color: #d32f2f;
-      color: #fff;
-    }
-
-    #header h1 {
-      font-size: 24px;
-      font-weight: normal;
-      margin: 0;
-    }
-
-    #menu {
-      display: flex;
-      align-items: center;
-      background-color: #fff;
-      box-shadow: 0 2px 4px rgba(0,0,0,0.2);
-      height: 60px;
-      padding: 0 20px;
-    }
-
-    #menu a {
-      color: #333;
-      text-decoration: none;
-      margin-right: 20px;
-      font-weight: bold;
-      font-size: 16px;
-    }
-
-    #menu a:hover {
-      color: #d32f2f;
-    }
-
-    #content {
-      display: flex;
-      flex-wrap: wrap;
-      margin: 20px;
-      padding: 20px;
-      background-color: #fff;
-      box-shadow: 0 2px 4px rgba(0,0,0,0.2);
-      border-radius: 5px;
-    }
-
-/* Estilos para los módulos */
-.module {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: calc(33.33% - 20px);
-  height: 180px;
-  margin: 10px;
-  background-color: #fff;
-  border-radius: 5px;
-  box-shadow: 0 4px 6px rgba(0,0,0,0.5), 0 1px 3px rgba(0,0,0,0.08);
-  text-align: center;
-  transition: transform 0.3s ease;
-}
-
-
-/* Enlaces en los módulos */
-.module a {
-  display: block;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  color: #fff;
-  text-decoration: none;
-}
-
-/* Estilos de los diferentes tipos de módulos */
-.module-pricing {
-  background-color: #4caf50;
-}
-
-.module-student {
-  background-color: #d32f2f;
-}
-
-.module-teacher {
-  background-color: #ff9800;
-}
-
-.module-class {
-  background-color: #e53935;
-}
-
-.module-grade {
-  background-color: #8bc34a;
-}
-
-/* Hover en los módulos */
-.module:hover {
-  transform: translateY(-10px);
-}
-
-/* Hover en los enlaces */
-.module a:hover {
-  opacity: 0.9;
-}
-
-/* Estilo para módulos seleccionados */
-.module.active {
-  transform: scale(1.05);
-  border: 2px solid #007bff;
-}
-
-
-    .module img {
-      max-width: 50%;
-      max-height: 50%;
-      margin: 10px;
-      border-radius: 100%;
-      background-color: #fff;
-      padding: 3px;
-      box-sizing: border-box;
-      font-size: 3rem;
-    }
-
-    @media screen and (max-width: 768px) {
-      #content {
-        margin: 10px;
-      }
-
-      .module {
-        width: calc(50% - 20px);
-      }
-    }
-
-    @media screen and (max-width: 480px) {
-      .module {
-        width:calc(100% - 20px);
-}
-}
-</style>
-
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div id="header">
     <h1>Colegio Cristiano Presbiteriano</h1>
   </div>
   <div id="menu">
-    <a href="#">Inicio</a>
-    <a href="#">Estudiantes</a>
+    <a href="admin.php">Inicio</a>
+    <a href="A_estudiante.php">Estudiantes</a>
     <a href="#">Mensualidad</a>
     <a href="#">Profesores</a>
     <a href="#">Cursos</a>
@@ -178,7 +31,7 @@ if (!isset($_SESSION['username']) || ($_SESSION['role'] != 'administrador' && $_
   </div>
   <div id="content">
     <div class="module module-student">
-      <a href="#">
+      <a href="A_estudiante.php">
         <img src="../../img/logo/graduado.png" alt="Estudiantes">
         <h2>Estudiantes</h2>
       </a>
