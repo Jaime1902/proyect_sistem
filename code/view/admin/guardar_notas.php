fe44c7f33dc060e8aa6f2fffd462b0d59f320526
Jaime1902 <112213180+Jaime1902@users.noreply.github.com>
2023-05-19 16:17:51 -0600

    0.0.5

--- code/view/admin/guardar_notas.php
+++ code/view/admin/guardar_notas.php
@@ -1,69 +1,58 @@
 <?php
-    // Conexión a la base de datos
-    include "../../conexion.php";
+// Conexión a la base de datos
+include "../../conexion.php";
 
-    // Comprobar la conexión
-    if ($conexion->connect_errno) {
-        echo 'Error en la conexión: ' . $conexion->connect_error;
-        exit();
-    }
+// Comprobar la conexión
+if ($conexion->connect_errno) {
+    echo 'Error en la conexión: ' . $conexion->connect_error;
+    exit();
+}
 
-    // Obtener los datos del grado y la asignatura seleccionados
-    $id_grado = $_GET['id_grado'];
-    $id_asignatura = $_GET['id_asignatura'];
+// Obtener los datos del grado y la asignatura seleccionados
+$id_grado = $_GET['id_grado'];
+$id_asignatura = $_GET['id_asignatura'];
 
-    // Recuperar las notas enviadas por el formulario
-    $notas_semestre1 = $_POST['semestre1'];
-    $notas_semestre2 = $_POST['semestre2'];
-    $notas_semestre3 = $_POST['semestre3'];
-    $notas_semestre4 = $_POST['semestre4'];
+// Recuperar las notas enviadas por el formulario
+$notas_semestre1 = $_POST['semestre1'];
+$notas_semestre2 = $_POST['semestre2'];
+$notas_semestre3 = $_POST['semestre3'];
+$notas_semestre4 = $_POST['semestre4'];
 
-    // Insertar o actualizar los registros de calificaciones para cada alumno
-    foreach ($notas_semestre1 as $id_alumno => $nota_semestre1) {
-        $nota_semestre2 = $notas_semestre2[$id_alumno];
-        $nota_semestre3 = $notas_semestre3[$id_alumno];
-        $nota_semestre4 = $notas_semestre4[$id_alumno];
+// Insertar o actualizar los registros de calificaciones para cada alumno
+foreach ($notas_semestre1 as $id_alumno => $nota_semestre1) {
+    $nota_semestre2 = $notas_semestre2[$id_alumno];
+    $nota_semestre3 = $notas_semestre3[$id_alumno];
+    $nota_semestre4 = $notas_semestre4[$id_alumno];
 
-        // Calcular el promedio final solo con las notas que se hayan enviado
-        $notas = array();
-        if ($nota_semestre1 !== "") {
-            $notas[] = intval($nota_semestre1);
+    // Verificar si el registro de calificaciones ya existe
+    $query = "SELECT COUNT(*) FROM calificaciones WHERE id_alumno='$id_alumno' AND id_grado='$id_grado' AND id_asignatura='$id_asignatura'";
+    $result = $conexion->query($query);
+    $row = $result->fetch_row();
+    $count = $row[0];
+
+    if ($count == 0) {
+        // El registro de calificaciones no existe, insertar un nuevo registro
+        $query = "INSERT INTO calificaciones (id_alumno, id_grado, id_asignatura, semestre1, semestre2, semestre3, semestre4, promedio_final) VALUES ('$id_alumno', '$id_grado', '$id_asignatura', ";
+        $query .= $nota_semestre1 !== "" ? "'".intval($nota_semestre1)."', " : "NULL, ";
+        $query .= $nota_semestre2 !== "" ? "'".intval($nota_semestre2)."', " : "NULL, ";
+        $query .= $nota_semestre3 !== "" ? "'".intval($nota_semestre3)."', " : "NULL, ";
+        $query .= $nota_semestre4 !== "" ? "'".intval($nota_semestre4)."', " : "NULL, ";
+        $query .= "NULL)";
+
+        // Ejecutar la consulta
+        if (!$conexion->query($query)) {
+            echo 'Error al guardar las notas: ' . $conexion->error;
+            exit();
         }
-        if ($nota_semestre2 !== "") {
-            $notas[] = intval($nota_semestre2);
-        }
-        if ($nota_semestre3 !== "") {
-            $notas[] = intval($nota_semestre3);
-        }
-        if ($nota_semestre4 !== "") {
-            $notas[] = intval($nota_semestre4);
-        }
-        $promedio_final = count($notas) > 0 ? array_sum($notas) / count($notas) : null;
-
-        // Verificar si el registro de calificaciones ya existe
-        $query = "SELECT COUNT(*) FROM calificaciones WHERE id_alumno='$id_alumno' AND id_grado='$id_grado' AND id_asignatura='$id_asignatura'";
-        $result = $conexion->query($query);
-        $row = $result->fetch_row();
-        $count = $row[0];
-
-        if ($count == 0) {
-            // El registro de calificaciones no existe, insertar un nuevo registro
-            $query = "INSERT INTO calificaciones (id_alumno, id_grado, id_asignatura, semestre1, semestre2, semestre3, semestre4, promedio_final) VALUES ('$id_alumno', '$id_grado', '$id_asignatura', ";
-            $query .= $nota_semestre1 !== "" ? "'".intval($nota_semestre1)."', " : "NULL, ";
-            $query .= $nota_semestre2 !== "" ? "'".intval($nota_semestre2)."', " : "NULL, ";
-            $query .= $nota_semestre3 !== "" ? "'".intval($nota_semestre3)."', " : "NULL, ";
-            $query .= $nota_semestre4 !== "" ? "'".intval($nota_semestre4)."', " : "NULL, ";
-            $query .= $promedio_final !== null ? "'".round($promedio_final, 2)."')" : "NULL)";
-        } else {
-            // El registro de calificaciones ya existe, actualizar el registro
-            $query = "UPDATE calificaciones SET ";
-            $query .= $nota_semestre1 !== "" ? "semestre1='".intval($nota_semestre1)."', " : "semestre1=NULL, ";
-            $query .= $nota_semestre2 !== "" ? "semestre2='".intval($nota_semestre2)."', " : "semestre2=NULL, ";
-            $query .= $nota_semestre3 !== "" ? "semestre3='".intval($nota_semestre3)."', " : "semestre3=NULL, ";
-            $query .= $nota_semestre4 !== "" ? "semestre4='".intval($nota_semestre4)."', " : "semestre4=NULL, ";
-            $query .= $promedio_final !== null ? "promedio_final='".round($promedio_final, 2)."'" : "promedio_final=NULL";
-            $query .= " WHERE id_alumno='$id_alumno' AND id_grado='$id_grado' AND id_asignatura='$id_asignatura'";
-        }
+    } else {
+        // El registro de calificaciones ya existe, no se actualiza ningún dato si ya tienen valor
+        $query = "UPDATE calificaciones SET ";
+        $query .= $nota_semestre1 !== "" ? "semestre1=IF(semestre1 IS NULL, '".intval($nota_semestre1)."', semestre1), " : "";
+        $query .= $nota_semestre2 !== "" ? "semestre2=IF(semestre2 IS NULL, '".intval($nota_semestre2)."', semestre2), " : "";
+        $query .= $nota_semestre3 !== "" ? "semestre3=IF(semestre3 IS NULL, '".intval($nota_semestre3)."', semestre3), " : "";
+        $query .= $nota_semestre4 !== "" ? "semestre4=IF(semestre4 IS NULL, '".intval($nota_semestre4)."', semestre4), " : "";
+        $query .= "promedio_final=NULL ";
+        $query .= "WHERE id_alumno='$id_alumno' AND id_grado='$id_grado' AND id_asignatura='$id_asignatura'";
 
         // Ejecutar la consulta
         if (!$conexion->query($query)) {
@@ -71,11 +60,12 @@
             exit();
         }
     }
+}
 
-    // Cerrar la conexión
-    $conexion->close();
+// Cerrar la conexión
+$conexion->close();
 
-    // Redirigir al usuario a la página de calificaciones
-    header('Location: evaluar.php?id_asignatura=' . $id_asignatura .'&id_grado=' . $id_grado);
-    exit();
+// Redirigir al usuario a la página de calificaciones
+header('Location: calificar.php?id_asignatura=' . $id_asignatura .'&id_grado=' . $id_grado);
+exit();
 ?>
